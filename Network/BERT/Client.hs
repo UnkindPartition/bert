9a2e6ff9a6eddca86ddab121b092bb8acd541f01
Roman Cheplyaka <roma@ro-che.info>
2013-11-04 15:17:26 +0200

    Trailing whitespace

--- Network/BERT/Client.hs
+++ Network/BERT/Client.hs
@@ -1,24 +1,24 @@
 -- |
 -- Module      : Network.BERT.Client
 -- Copyright   : (c) marius a. eriksen 2009
--- 
+--
 -- License     : BSD3
 -- Maintainer  : marius@monkey.org
 -- Stability   : experimental
 -- Portability : GHC
--- 
+--
 -- BERT-RPC client (<http://bert-rpc.org/>). This implements the
 -- client RPC call logic.
 
 module Network.BERT.Client
-  ( Call, call 
+  ( Call, call
   ) where
 
 import Data.Maybe
 import Data.BERT (Term(..), Packet(..), BERT(..))
 import Network.BERT.Transport (Transport, withTransport, sendt, recvt)
 
-data Error 
+data Error
   = ClientError String
   | ServerError Term
     deriving (Show, Ord, Eq)
@@ -30,21 +30,21 @@ type Call a = IO (Either Error a)
 -- defined by @transport@, returning the results of the call or an
 -- error.
 call :: (BERT a, BERT b)
-     => Transport 
-     -> String 
+     => Transport
+     -> String
      -> String
      -> [a]
      -> Call b
-call transport mod fun args = 
+call transport mod fun args =
   withTransport transport $ do
-    sendt $ TupleTerm [AtomTerm "call", AtomTerm mod, AtomTerm fun, 
+    sendt $ TupleTerm [AtomTerm "call", AtomTerm mod, AtomTerm fun,
                        ListTerm $ map showBERT args]
     recvAndHandle
   where
     handle (TupleTerm [AtomTerm "reply", reply]) =
       return $ either (const . Left $ ClientError "decode failed") Right
              $ readBERT reply
-    handle (TupleTerm (AtomTerm "info":_)) = 
+    handle (TupleTerm (AtomTerm "info":_)) =
       recvAndHandle  -- We don't yet handle info directives.
     handle t@(TupleTerm (AtomTerm "error":_)) =
       return $ Left . ServerError $ t
